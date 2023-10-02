05b74c95a790c013ee9c5510f8503ac25aeb6b2a
BirgitKarlhuber <birgit@karlhuber.net>
2023-10-02 16:51:39 +0200

    setup package for lab 5

---
+++ R/request_API.R
@@ -0,0 +1,19 @@
+#' Title
+#' 
+#' Description
+#'
+#' @param xx description
+#' 
+#' @return description
+#' 
+#' @export
+#' 
+#' @examples
+#' library(Lab5)
+#' request_API(x)
+
+request_API <- function(){
+  
+  
+  
+}

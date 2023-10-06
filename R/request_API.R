52a6b0bb96ba1accc62435a2b04131e665935191
BirgitKarlhuber <birgit@karlhuber.net>
2023-10-06 11:11:10 +0200

    modified request

--- R/request_API.R
+++ R/request_API.R
@@ -2,15 +2,9 @@
 #' 
 #' This function can be used to fetch data from the Thenmaps-API. 
 #'
-#' @param version There are two version of the data available "v1" and "v2". 
-#' 
 #' @param dataset The dataset is a area-level. For the area, the ISO 3166 codes where available was used, and for level, the ten levels of administrative borders from Open Streetmap was used. 
 #' 
-#' @param modules There are 4 different modules available: data, info, geo, svg
-#' 
 #' @param date Can be on the form YYYY-MM-DD, YYYY-MM, or YYYY. The output will include only areas that existed at that very date. Dates in the database are local time and inclusive, meaning that 1947-03-01 will include a nation that gained independence on April 1 1947, according to the local timezone. 2011-05 will be interpreted as 2001-05-01, and 2011 will be interpreted as 2001-01-01. 
-#' 
-#' @param language Some parts of the data are available in different languages, such as names of political entities. 
 #' 
 #' @return The function returns the requested dataset.
 #' 
@@ -20,7 +14,7 @@
 #' 
 #' @examples
 #' library(Lab5)
-#' requested_data <- request_API("v1","world-2","geo","2016-01-01","fi")
+#' requested_data <- request_API("world-2", "2016-01-01")
 #' 
 #' @seealso \url{https://www.thenmap.net/}
 
@@ -29,84 +23,29 @@
 # library(httr)
 # library(rjson)
 
-request_API <- function(version,dataset,modules,date,language) {
+request_API <- function(dataset,date) {
   
-  stopifnot(version %in% c("v1","v2"), 
-            dataset %in% c("ch-8", "no-7", "no-4", "dk-7", "se-7", "se-4", "us-4", "gl-7", "world-2"),
-            modules %in% c("geo"), # modules %in% c("data","geo"),
-            nchar(language) %in% c(2,3))
+  stopifnot(dataset %in% c("ch-8", "no-7", "no-4", "dk-7", "se-7", "se-4", "us-4", "gl-7", "world-2"))
   
-  api_url <- paste0(paste0("http://api.thenmap.net/", version, "/", dataset, "/", modules, "/", date, 
-                           "?gep_props=name&language=", language))
+  api_url <- paste0(paste0("http://api.thenmap.net/v1/", dataset, "/geo/", date, 
+                           "?gep_props=name&language=en"))
   
   response <- GET(api_url)
   
   # check if the request was successful (status code 200)
   if (status_code(response) == 200) {
     
-    if(modules == "data"){
-      
-      myData <- rjson::fromJSON(file=api_url)
-      data_frame_json <- as.data.frame(myData)
-      # str(data_frame_json) # still needs to be formated a little bit
-      
-      return(data_frame_json)
-      
-    } else if(modules == "geo") {
-      geojson_content <- content(response, "text")
-      sf_object <- st_read(geojson_content)
-      return(sf_object)
-    }
+    geojson_content <- content(response, "text")
+    sf_object <- st_read(geojson_content)
+    return(sf_object)
+    
   } else {
     stop("Error fetching data with status code: ", status_code(response))
   }
 }
 
-# version <- "v1" # v1, v2
 # dataset <- "world-2" # ch-8 no-7 no-4 dk-7 se-7 se-4 us-4 gl-7 world-2
-# modules <- "geo" # data (info) geo (svg)
 # date <- "2016-01-01" # YYYY-MM-DD, YYYY-MM, or YYYY
-# language <- "fi"
-# 
-# requested_data <- request_API(version,dataset,modules,date,language)
 
-
-####
-
-
-# version <- "v2" # v1, v2
-# dataset <- "se-7" # ch-8 no-7 no-4 dk-7 se-7 se-4 us-4 gl-7 world-2
-# modules <- "data" # data (info) geo (svg)
-# date <- "1974" # YYYY-MM-DD, YYYY-MM, or YYYY
-# language <- "se"
-# 
-# 
-# 
-# api_url <- paste0(paste0("http://api.thenmap.net/", version, "/", dataset, "/", modules, "/", date, 
-#                          "?gep_props=name&language=", language))
-# 
-# response <- GET(api_url)
-# 
-# 
-# myData <- rjson::fromJSON(file=api_url)
-# data_frame_json <- as.data.frame(myData)
-# 
-# 
-# length(data_frame_json)
-# 
-# odd <- seq(1,(length(data_frame_json)),2)
-# even <- seq(2,(length(data_frame_json)),2)
-# 
-# 
-# col1 <- unname(unlist(data_frame_json[,odd]))
-# col2 <- unname(unlist(data_frame_json[,even]))
-# 
-# 
-# data <- data.frame(col1,col2)
-# names(data) <- c(names(data_frame_json[1]), names(data_frame_json[2]))
-# head(data)
-# 
-# head(data_frame_json)
-# 
-# 
+# requested_data <- request_API(dataset,date)
 
--- tests/testthat/test_API.R
+++ tests/testthat/test_API.R
@@ -1,13 +1,12 @@
 
 test_that("function rejects errounous input", {
-  expect_error(request_data <- request_API("v3","world-2","geo","2016-01-01","fi"))
-  expect_error(request_data <- request_API("v2","at-3","geo","2016-01-01","fi"))
-  expect_error(request_data <- request_API("v2","at-3","gee","2016-01-01","fi"))
-  # expect_error(request_data <- request_API("v2","at-3","geo","01-01-2016","fi"))
-  expect_error(request_data <- request_API("v2","at-3","geo","2016-01-01","dfea"))
+  expect_error(request_data <- request_API("at-3", "2016-01-01"))
+  # expect_error(request_data <- request_API("world-2","01-01-2016"))
+ 
 })
 
 
+# change them 
 test_that("outputs are correct", {
   expect_equal(round(request_API("v2","world-2","geo","2016-01-01","fi")$geometry[[1]][[1]][[1]][1:3,],2),
                round(rbind(c(-58.17262, 6.812218),c(-57.96667, 6.775418), c(-57.51919,6.270764)),2))
